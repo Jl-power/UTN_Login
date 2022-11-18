f77a246051ea7109c2b33f6e9ad290ff7d8e1c1a
lucas <lucasperez381@gmail.com>
2022-11-18 17:02:10 -0300

    Home design

--- app/src/main/java/com/example/utn_login/HomeActivity.kt
+++ app/src/main/java/com/example/utn_login/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.example.utn_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.utn_login.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
